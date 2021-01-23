03c6d024f41c4e3a59f489e86b8ed272161612ee
Nicholas Crumrine <uncletammy@gmail.com>
2021-01-23 14:13:59 -0600

    adds zipped function

---
+++ all-teh-lambdas/function.js
@@ -0,0 +1,37 @@
+const AWS = require('aws-sdk');
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+const twilio = require('twilio');
+
+exports.handler = async (event, context) => {
+    let body = JSON.parse(event.body);
+
+    const accountSid = process.env.TWILIO_SID;
+    const authToken = process.env.TWILIO_TOKEN;
+
+    const client = twilio(accountSid, authToken);
+    let responseObject = {
+        statusCode: undefined,
+        body: {}
+    }
+
+    client.messages
+    .create({
+        body: body.words,
+        from: '+14252767041',
+        to: '+18479247616'
+    })
+    .then( (message) => {
+        responseObject.body = message;
+        responseObject.statusCode = 200;
+        console.log(message.sid);
+    })
+    .catch( (error) => {
+        responseObject.body = error;
+        responseObject.statusCode = 500;
+        console.log(error);
+    });
+   
+    return responseObject;
+};
